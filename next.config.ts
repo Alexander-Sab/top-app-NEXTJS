c837bda19e2414b3e8c01793e59437b4419729f1
Alexander-Sab <113023875+Alexander-Sab@users.noreply.github.com>
2024-12-07 15:38:22 +0300

    сообщенеи коммита

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+	/* config options here */
+	reactStrictMode: true
 };
 
 export default nextConfig;
+
+module.exports = {
+	webpack(config: {
+		module: {
+			rules: { test: RegExp; issuer: { and: RegExp[] }; use: string[] }[];
+		};
+	}) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: {
+				and: [/\.(js|ts)x?$/]
+			},
+			use: ['@svgr/webpack']
+		});
+		return config;
+	}
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
+import { Html, Head, Main, NextScript } from 'next/document';
+//1
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="ru">
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap"
+					rel="stylesheet"
+				></link>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
